977f227b924d3342c494b7d124375df5031d9b60
Ahmed <Ahmed@DESKTOP-7U319EG>
2025-04-11 22:35:57 +0200

    Add project files.

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity , TKay> where TEntity : BaseEntity<TKay>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool trackChanges = false);
+        Task<TEntity?> GetByIdAsync(TKay id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ Core/Services/Mapping Profiles/ProductProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Models;
+using Shared;
+
+namespace Services.Mapping_Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.BrandName, opt => opt.MapFrom(src => src.ProductBrand.Name))
+                .ForMember(dest => dest.TypeName, opt => opt.MapFrom(src => src.ProductType.Name));
+
+            CreateMap<ProductBrand, BrandDto>();
+            CreateMap<ProductType, TypeDto>();
+
+        }
+    }
+}
